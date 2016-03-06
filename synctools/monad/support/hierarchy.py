77578a20dbe7d8a362eba408623befc7cec46674
Oakland Peters <oakland.peters@gmail.com>
2016-03-06 16:03:14 -0500

    Refactor: future hierarchy notes. * Much cleaner logic for Elm/Morph __rshift__ * Added __matmul__, to support functor map/bind * Naming change: map is now f_apply/Haskell-bind bind is now f_compose/Haskell-Kleisli-compose

--- synctools/monad/support/hierarchy.py
+++ synctools/monad/support/hierarchy.py
@@ -39,17 +39,58 @@ class Morphism(metaclass=abc.ABCMeta):
 
 class MonadicMorphismInterface(Morphism):
     @abstractclassproperty
-    def Category(cls) -> MonadInterface:
+    def Codomain(cls) -> MonadInterface:
+        """For Monads, the category is basically the monad itself."""
+        return NotImplemented
+
+    @abstractclassproperty
+    def Domain(cls) -> 'CategoryInterface':
+        """Domain will almost always be Pysk."""
         return NotImplemented
 
     @abstractpedanticmethod
-    def map(cls, morphism: 'cls.Category.Morphism', element: 'Pysk.Element') -> 'cls.Category.Morphism':
+    def map(cls,
+            element: 'cls.Codomain.Element',
+            constructor: 'Callable[cls.Domain.Element, [cls.Codomain.Element]]'
+            ) -> 'cls.Codomain.Element':
+        """
+        In Haskell, this is called 'bind', using symbol: >>=
+        'f_apply' ~ functor-specific apply
+
+        (C1, D1 -> C2)  ->  C2
+        """
         return NotImplemented
 
-    @abstractpednanticmethod
-    def bind(cls, morphism: 'cls.Category.Morphism', constructor) -> 'cls.Category.Morphism':
-        """Constructor accepts Psyk.Element and return Category.Element."""
+    @abstractpedanticmethod
+    def bind(cls,
+             morphism: 'cls.Codomain.Morphism',
+             constructor: 'Callable[cls.Domain.Element, [cls.Codomain.Element]]'
+             ) -> 'cls.Codomain.Element':
+        """
+        In Haskell, this is called 'Klesli composition', using symbol: >=>
+        'f_compose' ~ functor-specific composotion
+
+        (C1 -> C2, D2 -> C3) -> C3
+        """
         return NotImplemented
+
+    @abstractpedanticmethod
+    def f_apply(cls,
+                element: 'cls.Codomain.Element',
+                constructor: 'Callable[cls.Domain.Element, [cls.Codomain.Element]]'
+                ) -> 'cls.Codomain.Element':
+        """This is an alias of the bind function."""
+        return cls.bind(element, constructor)
+
+    @abstractpedanticmethod
+    def f_compose(cls,
+                  morphism: 'cls.Codomain.Morphism',
+                  constructor: 'Callable[cls.Domain.Element, [cls.Codomain.Element]]'
+                  ) -> 'cls.Codomain.Morphism':
+        """This is kleisli-composition, which is basically the function-to-function
+        version of the bind function."""
+        return NotImplemented
+
 
 class MonadicElementInterface(MonadInterface, Element):
     @abstractclassproperty
@@ -59,39 +100,54 @@ class MonadicElementInterface(MonadInterface, Element):
 
 class MonadicMorphism(MonadicMorphismInterface):
     """Placeholder for an idea"""
-    def __call__(self, element):
+    @abstractclassproperty
+    def Category(cls) -> MonadInterface:
+        return NotImplemented
+
+    def __call__(self, element: 'cls.Codomain.Element') -> 'cls.Codomain.Element':
         return self.map(element)
 
     @pedanticmethod
-    def __rshift__(cls, self: 'Element', arg) -> 'Union[cls.Category.Element, cls.Category.Morphism]':
-        if isinstance(arg, cls.Category.Morphism):
-            return self.compose(arg)
-        elif isinstance(arg, cls.Category.Element):
-            return self.call(arg)
+    def __rshift__(cls, self: 'Morphism', arg) -> 'Union[cls.Codomain.Element, cls.Codomain.Morphism]':
+        if isinstance(arg, cls.Codomain.Morphism):
+            return cls.compose(self, arg)
+        elif isinstance(arg, cls.Codomain.Element):
+            return cls.call(self, arg)
+        elif isinstance(arg, Pysk.Morphism):
+            return cls.compose(self, cls.decorate(arg))
+        elif isinstance(arg, Pysk.Element):
+            return cls.call(self, cls.construct(arg))
         else:
-            if isinstance(arg, Pysk.Morphism):
-                return self.bind(arg)
-            elif isinstance(arg, Pysk.Element):
-                return self.map(arg)
-            else:
-                raise _category_mismatch_error(cls, self, arg)
+            raise _category_mismatch_error(cls, self, arg)
+
+    @pedanticmethod
+    def __matmul__(cls, self: '', constructor: 'Callable[cls.Domain.Element, [cls.Codomain.Element]]') -> 'cls.Codomain.Morphism':
+        """This is kleisli-composition, which is basically the function-to-function
+        version of the bind function."""
+        return cls.f_compose(self, constructor)
+
 
 
 class MonadicElement(MonadicElementInterface):
     """Placeholder for any idea"""
     @pedanticmethod
-    def __rshift__(cls, self: 'MonadicElement', arg) -> 'Union[Pysk.Element, Pysk.Morphism]':
-        if isinstance(arg, cls.Category.Element):
-            return self.append(arg)
-        elif isinstance(arg, cls.Category.Morphism):
-            return self.apply(arg)
+    def __rshift__(cls, self: 'MonadicElement', arg) -> 'Union[cls.Domain.Element, cls.Domain.Morphism]':
+        if isinstance(arg, cls.Codomain.Morphism):
+            return cls.apply(self, arg)
+        elif isinstance(arg, cls.Codomain.Element):
+            return cls.append(self, arg)
+        elif isinstance(arg, Pysk.Morphism):
+            return cls.apply(self, cls.decorate(arg))
+        elif isinstance(arg, Pysk.Element):
+            return cls.append(self, cls.construct(arg))
         else:
-            if isinstance(arg, Pysk.Morphism):
-                return self.bind(arg)
-            elif isinstance(arg, Pysk.Element):
-                return self.map(arg)
-            else:
-                raise _category_mismatch_error(cls, self, arg)
+            raise _category_mismatch_error(cls, self, arg)
+
+    @pedanticmethod
+    def __matmul__(cls, self: 'Element', constructor: 'Callable[cls.Domain.Element, [cls.Codomain.Element]]') -> 'cls.Domain.Morphism':
+        """This is the bind function."""
+        return cls.f_call(self, constructor)
+
 
 
 #
@@ -102,13 +158,13 @@ def try_to_append(self, arg, cls):
     """
     # The logic in this step could be made tighter
     # I think it's mostly correct, but I would like to verify
-    # isinstance(self, X), isinstance(arg, X), issubclass(cls.Category.Element, X)
+    # isinstance(self, X), isinstance(arg, X), issubclass(cls.Codomain.Element, X)
     if isinstance(self, MonoidalElement):
         return self.append(arg)
     else:
         raise TypeError(str.format(
             "Element >> Element is invalid, because {0} is not a Monoidal Category.",
-            cls.Category.Element.__name__
+            cls.Codomain.Element.__name__
         ))
 
 def _category_mismatch_error(cls, self, arg):
@@ -117,5 +173,5 @@ def _category_mismatch_error(cls, self, arg):
         "'{1}' in category '{2}'.",
         arg.__class__.__name__,
         self.__class__.__name__,
-        cls.Category.__name__
+        cls.Codomain.__name__
     ))
