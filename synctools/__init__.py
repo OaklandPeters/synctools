40432d5eb14c4e022e6c897e4d7c139fe333b376
Oakland Peters <oakland.peters@gmail.com>
2016-04-13 17:17:26 -0400

    Working toward py3 compatibility

--- synctools/__init__.py
+++ synctools/__init__.py
@@ -1,3 +1,4 @@
-import read_html_minimal
-import read_html
-import read_html_simple
+# import read_html_minimal
+# import read_html
+from . import read_html_simple
+from . import read_html_dry_run
--- synctools/read_html_simple.py
+++ synctools/read_html_simple.py
@@ -2,25 +2,36 @@
 One-file version, few dependencies
 """
 import re
-import urllib2
+# import urllib2
 from functools import partial
 
-from lxml import html, cssselect
+from lxml import html
+# hacky Python2/3 compatibility
+try:
+    from lxml.cssselect import CSSSelector
+except ImportError:
+    from cssselect import Selector as CSSSelector
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
 
-from sync_media_function import sync_media
-from metafuncs import branch, combine, maybe, tryit, getitem, cache, Pipe
+from .sync_media_function import sync_media
+from .metafuncs import branch, combine, maybe, tryit, getitem, cache, Pipe
 
 
 # Support functions
 is_not_none = lambda obj: obj is not None
 unique = lambda sequence: list(set(sequence))
 get_html = html.parse  # :: str -> ElementTree
-img_tags = cssselect.CSSSelector('img')  # :: ElementTree -> List[Element]
+# img_tags = cssselect.CSSSelector('img')  # :: ElementTree -> List[Element]
+img_tags = CSSSelector('img')  # :: ElementTree -> List[Element]
 get_src = maybe(getitem('src'),     # normal
             maybe(getitem('data-src'),  # photo pages
                 maybe(getitem('data-srcset'))))  # home page
 BACKGROUND_IMAGE_REGEX = re.compile(r'background-image: url\((.*?\))')
-read_page = lambda url: urllib2.urlopen(url).read()
+# read_page = lambda url: urllib2.urlopen(url).read()
+read_page = lambda url: urlopen(url).read()
 PATH_SPLIT_REGEX = re.compile(r'^(/media)/(.*)/(.*?\..*?)$')
 # PATH_SPLIT_REGEX = re.compile(r'/media/(.*)/(')
 split_path_parts = lambda src: PATH_SPLIT_REGEX.match(src).groups()  # :: str -> PathPats
@@ -39,6 +50,10 @@ remove_prefix = maybe(crop('http://opeterml1297110.njgroup.com:7000'),
                             maybe(crop('https://cdn.theatlantic.com/assets/'),
                                   lambda word: word)))
 
+#   NOT YET USED
+# Read data input, with no processing
+data_read = (Pipe() >> branch(get_html, read_page) >> cache)
+
 # Composite work-horse functions
 # Retreive src-like properties from <img> tags
 get_img_srcs = (
@@ -47,10 +62,12 @@ get_img_srcs = (
     >> img_tags  # :: List[Element]
     >> partial(map, get_src)  # List[Optional[Path]]
 )
+
 # Retreive URL-paths from CSS 'background-image:' properties
 get_css_srcs = (
     Pipe()  # :: Location
-    >> read_page  # :: str
+    >> read_page  # :: byte
+    >> str   # :: str
     >> BACKGROUND_IMAGE_REGEX.findall  # :: List[str]
 )
 # Format relative paths for sync-media
